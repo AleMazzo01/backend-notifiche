93d4e23671c0135208e87b20f85c5cd0744298d1
Alessandro Mazzini <alessandro.mazzini.31@gmail.com>
2025-01-13 23:23:38 +0100

    invio notifiche ogni 3 ore dalle 7 alle 22

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const sendPushNotification = async (token) => {
 
 
 
-cron.schedule('*/2 * * * *', () => {
+cron.schedule('0 7-22/3 * * *', async () => {
     console.log('Invio notifiche cicliche...');
     console.log('Token registrati:', tokens); // Aggiungi questo log
 
